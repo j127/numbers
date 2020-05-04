e94190ef8081e2226107f7a1508e778adfc25b10
J. Cohen <hostelmarketing@gmail.com>
2020-05-03 19:24:33 -0700

    Add alternate syntax

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@ pub fn say_hello() {
 }
 
 pub fn print() {
+    // alternate syntaxes
+    // let numbers = [1u8, 2, 3, 4, 5];
+    // let numbers: [u8; 5] = [1, 2, 3, 4, 5];
     let numbers = [1, 2, 3, 4, 5];
     for n in numbers.iter() {
         println!("{}", n);
