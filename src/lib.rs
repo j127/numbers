322ef3f87c8cc1d49e21e10a783d81b3a1a562d5
J. Cohen <hostelmarketing@gmail.com>
2020-05-03 19:30:07 -0700

    Using vec

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,19 @@ pub fn say_hello() {
 }
 
 pub fn print() {
-    // alternate syntaxes
+    // alternate syntax for arrays
     // let numbers = [1u8, 2, 3, 4, 5];
     // let numbers: [u8; 5] = [1, 2, 3, 4, 5];
-    let numbers = [1, 2, 3, 4, 5];
-    for n in numbers.iter() {
+    // let numbers = [1, 2, 3, 4, 5];
+
+    // using Vec<T> instead of an array
+    let numbers = vec![1, 2, 3, 4, 5];
+
+    // a hack to view the type by causing a compiler error
+    // let () = numbers;
+
+    // use `.iter()` for an array, but it isn't required for vec
+    for n in numbers {
         println!("{}", n);
     }
 }
